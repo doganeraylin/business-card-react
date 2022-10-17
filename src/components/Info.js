7d31571749c913de7231d1fdcd4f7df77c4680d8
aylindoganer <doganeraylinn@gmail.com>
2022-10-17 20:42:55 +0300

    components are created

---
+++ src/components/Info.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+export default function Info() {
+    return (
+        <div>
+            <img src="" />
+            <h1>Laura Smith</h1>
+            <h2>Frontend Developer</h2>
+            <h3>www.example.org</h3>
+            <div className="buttons">
+                <button>Email</button>
+                <button>Linkedin</button>
+            </div>
+        </div>
+    )
+}
---
+++ src/components/Interests.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Interests() {
+    return (
+        <div>
+            <p>Interests</p>
+            <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco</p>
+        </div>
+    )
+}
